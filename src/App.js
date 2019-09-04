e45de3fd86735623955401a7e1ccf36770edf861
Timothy Regan <tregan@pillartechnology.com>
2019-09-04 16:24:57 -0400

    remove code splitting from library itself and let consumer code handle it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 
 import './App.scss'
 
-import React, { Suspense, lazy } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
 import DataSetListViewWrapper from './pages/dataset-list-view'
 import DatasetView from './pages/dataset-detail-view'
-const DatasetVisualizationView = lazy(() => import('./pages/dataset-visualization-view'))
+import DatasetVisualizationView from './pages/dataset-visualization-view'
 import LoginView from './pages/login-view'
 
 import NetworkLoadingElement from './components/network-loading-element'
-import LoadingElement from './components/generic-elements/loading-element'
 
 import routes from './routes'
 
@@ -22,16 +21,14 @@ const DiscoveryUI = () => (
   <main-app-element>
     <NetworkLoadingElement />
     <Router>
-      <Suspense fallback={<LoadingElement />}>
-        <Switch>
-          <Route exact path={routes.root} component={DataSetListViewWrapper} />
-          <Route exact path={routes.datasetView} component={DatasetView} />
-          <Route exact path={routes.datasetVisualizationView} component={props => <DatasetVisualizationView {...props} />} />
-          <Route exact path={routes.healthCheck} component={() => <div>Everything is fine</div>} />
-          <Route exact path={routes.login} component={LoginView} />
-          <Route component={noMatch} />
-        </Switch>
-      </Suspense>
+      <Switch>
+        <Route exact path={routes.root} component={DataSetListViewWrapper} />
+        <Route exact path={routes.datasetView} component={DatasetView} />
+        <Route exact path={routes.datasetVisualizationView} component={props => <DatasetVisualizationView {...props} />} />
+        <Route exact path={routes.healthCheck} component={() => <div>Everything is fine</div>} />
+        <Route exact path={routes.login} component={LoginView} />
+        <Route component={noMatch} />
+      </Switch>
     </Router>
   </main-app-element>
 )
