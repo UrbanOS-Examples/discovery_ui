9f00452309578aebcf05c332612f655c4fdc9701
Timothy Regan <tregan@pillartechnology.com>
2021-01-19 15:08:07 -0500

    config fix

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 cat > /usr/share/nginx/html/config.js <<EOL
 window.API_HOST = '${API_HOST}'
+window.CONTRIBUTE_HOST = '${CONTRIBUTE_HOST}'
 window.GTM_ID = '${GTM_ID}'
 window.BASE_URL = '${BASE_URL}'
 window.STREETS_TILE_LAYER_URL = '${STREETS_TILE_LAYER_URL}'
