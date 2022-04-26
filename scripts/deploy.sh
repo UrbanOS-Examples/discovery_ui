d03fa278d964eb1198b1d27f898571c89360ae25
Timothy Regan <tregan@pillartechnology.com>
2022-04-26 14:40:19 -0400

    update github actions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 RELEASE_TYPE=$1
-echo "This script needs to be updated for a default demo deployment"
-# echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+# echo "This script needs to be updated for a default demo deployment"
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
-# echo "Determining image tag for ${GITHUB_BRANCH} build ..."
+echo "Determining image tag for ${GITHUB_BRANCH} build ..."
 
-# if [[ $RELEASE_TYPE == "release" ]]; then
-#     export TAGGED_IMAGE="smartcolumbusos/discovery_ui:${GITHUB_BRANCH}"
-# elif [[ $RELEASE_TYPE == "master" ]]; then
-#     export TAGGED_IMAGE="smartcolumbusos/discovery_ui:development"
-# else
-#     echo "Branch should not be pushed to Dockerhub"
-#     exit 0
-# fi
+if [[ $RELEASE_TYPE == "release" ]]; then
+    export TAGGED_IMAGE="smartcitiesdata/discovery_ui:${GITHUB_BRANCH}"
+elif [[ $RELEASE_TYPE == "master" ]]; then
+    export TAGGED_IMAGE="smartcitiesdata/discovery_ui:development"
+else
+    echo "Branch should not be pushed to Dockerhub"
+    exit 0
+fi
 
-# echo "Pushing to Dockerhub with tag ${TAGGED_IMAGE} ..."
+echo "Pushing to Dockerhub with tag ${TAGGED_IMAGE} ..."
 
-# docker tag discovery_ui:build "${TAGGED_IMAGE}"
-# docker push "${TAGGED_IMAGE}"
+docker tag discovery_ui:build "${TAGGED_IMAGE}"
+docker push "${TAGGED_IMAGE}"
